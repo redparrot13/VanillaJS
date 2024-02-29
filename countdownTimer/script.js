e54b0af03d934342526016291a9e1bacd524437d
Kathleen Wilson <k.wilson467@gmail.com>
2024-02-28 18:03:59 -0600

    logic

---
+++ countdownTimer/script.js
@@ -0,0 +1,41 @@
+const currentDate = new Date();
+//const targetDate = new Date('2024-03-20T00:01:00');
+const targetDate = new Date('2024-06-18T08:10:00');
+
+//console.log(targetDate)
+
+const difference = targetDate - currentDate;
+
+
+//console.log(difference);
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const difference = targetDate - currentTime;
+    const days = Math.floor(difference / ( 1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    /*console log tests: 
+    console.log("days: ", days);
+    console.log("hours: ", hours);
+    console.log("minutes: ", minutes);
+    console.log("seconds: ", seconds); 
+    */
+
+    if(difference < 0){
+        clearInterval(interval);
+        document.getElementById("timer").innerText = "HAPPY BIRTHDAY!!!!!"
+    }
+
+}
+
+const interval = setInterval(updateCountdown, 1000);
+
+
