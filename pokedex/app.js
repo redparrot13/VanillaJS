2160d063c165ac11dbc83b9dbd9f3f9377f494b4
Kathleen Wilson <redparrot13@gmail.com>
2024-03-26 12:18:10 -0500

    promises

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,19 +1,25 @@
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)
-        .then(res => {
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                type: data.types.map(type => type.type.name).join(', ')
-            };
-            console.log(pokemon)
-        })
-}
+
+    const promises = [];
+
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then( results => {
+        const pokemon = results.map( (data) => ({
+
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map(type => type.type.name).join(', ')
+
+        }));
+        console.log(pokemon)
+    });
+
+    
+};
 
 fetchPokemon();
