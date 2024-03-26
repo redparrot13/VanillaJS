2bc92b721f9784c1b95b85512b1f44c893e7472f
Kathleen Wilson <redparrot13@gmail.com>
2024-03-26 11:51:36 -0500

    fetch

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,5 +1,19 @@
 const fetchPokemon = () => {
-
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    fetch(url)
+        .then(res => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const pokemon = {
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                type: data.types.map(type => type.type.name).join(', ')
+            };
+            console.log(pokemon)
+        })
 }
 
 fetchPokemon();
